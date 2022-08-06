0cc3d45c191485df065a878ed4daaecfbe99fbd0
laila146 <110485375+laila146@users.noreply.github.com>
2022-08-06 20:31:35 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,53 @@
+let Tilfeldig_tall = 0
+input.onGesture(Gesture.Shake, function () {
+    Tilfeldig_tall = randint(1, 6)
+    if (Tilfeldig_tall == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Tilfeldig_tall == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (Tilfeldig_tall == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (Tilfeldig_tall == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (Tilfeldig_tall == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . . . . .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
